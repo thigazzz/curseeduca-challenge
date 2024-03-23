1978e883d03deaf39a07cc063fa31dfaa16ec091
thigazzz <thiago.p.dasilva2005@gmail.com>
2024-03-23 08:38:05 -0300

    feat: search bar in header

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SearchBar from "./components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="w-screen bg-slate-200 flex justify-around md:justify-between items-center sm:p-4">
+          <div>Capucceeno</div>
+          <div>
+            <div>
+              <SearchBar/>
+            </div>
+            <div>
+              Carrinho
+            </div>
+          </div>
+        </header>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
